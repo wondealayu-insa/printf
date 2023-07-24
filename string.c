7e82f8a2622969d972e45e786ce644bee46fe535
chiro999 <r.o.g.u.e@outlook.com>
2023-07-24 05:34:23 -0700

    100

---
+++ string.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_string - writes the character c to stdout
+ * @s: The string to print
+ *
+ * Return: 1.
+ */
+int _string(va_list str)
+{
+	char string;
+	int  i = 0;
+
+	string = va_arg(s, char *);
+	if (string == NULL)
+		string = "(nill)";
+	while (string[i] != 0)
+	{
+		_putchar(string[i]);
+		i++;
+	}
+	return (i);
+}
