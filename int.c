3bb2d50dd8b192f5aefc9fff53bd6282b8645aa0
wondealayu-insa <wondealayu@gmail.com>
2023-07-24 04:29:44 -0700

    int.c & rot13.c

---
+++ int.c
@@ -0,0 +1,55 @@
+#include "main.h"
+/**
+ * printf_int - prints integer
+ * @args: argument to print
+ * Return: number of characters printed
+ */
+int _int(va_list integer)
+{
+	int n = va_arg(integer, int);
+	int num, last_digit = n % 10, digit, exp = 1;
+	int  i = 0;
+	
+	/* obtain all digits but the last one */
+	n = n / 10;
+	num = n;
+	
+	/**handling negative integers
+	 * Prints the - character and sets all numbers to 0
+	*/ 
+	if (last_digit < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last_digit = -last_digit;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+​
+		/* while loop to separate digits */
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+​
+			/* sets the next digit to be printed */
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+​
+	/* adds the last character to the rest of the digits */
+	_putchar(last_digit + '0');
+	i++;
+​
+	return (i);
+}
---
+++ rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * _rot13 - encryptes plaintext to ciphertext using rot13 cipher
+  * @plaintext: string to convert
+  * Return: size of ciphertext
+  */
+int _rot13(va_list plaintext)
+{
+	int j = 0, i = 0, size = 0;
+	char *str;
+	char normal[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char shifted[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+​
+	str = va_arg(plaintext, char *);
+	if (str == NULL)
+		str = "(nothing_to_encrypt)";
+	for (;str[i] != '\0'; i++)
+	{
+		for (;normal[j] != '\0'; j++)
+		{
+			if (str[i] == normal[j])
+			{
+				_putchar(shifted[j]);
+				size++;
+				break;
+			}
+		}
+	}
+	return (size);
+}
